8abba2f4c81b52eb4df1ccb78491ddb5f7d2fcd8
msoviche <maximilien.soviche@csgroup.eu>
2023-11-14 00:24:54 +0100

    [create] configuration files for installation, tests and pre commit config

---
+++ setup.py
@@ -0,0 +1,11 @@
+# Copyright notice
+
+"""
+setup.py kept for compatibility and setuptools_scm configuration.
+Main part is in setup.cfg file.
+"""
+
+from setuptools import setup
+
+# Main setup with setup.cfg file.
+setup(use_scm_version=True)
